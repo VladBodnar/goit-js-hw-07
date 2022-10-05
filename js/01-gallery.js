88cd226d38f3a74bbdca733b2b68c1fbc6a17570
VladBodnar <104225683+VladBodnar@users.noreply.github.com>
2022-10-06 01:01:54 +0300

    final v2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
+const headStyleLiinks =document.querySelector('head');
+const bodyScriptref =document.querySelector('body');
+const galleryBox = document.querySelector(".gallery");
 
 
- const galleryBox = document.querySelector(".gallery");
+const divGaleryArray = [];
+
+galleryItems.map((imgObjekt) => {
+   const divGaleryItem = document.createElement('div');
+   const linkGaleryItem = document.createElement('a');
+   const imgGaleryItem = document.createElement('img');
+   divGaleryItem.class="gallery__item";
+   linkGaleryItem.class="gallery__link";
+   linkGaleryItem.href=imgObjekt.original;
+   imgGaleryItem.src=imgObjekt.preview;
+   imgGaleryItem.alt=imgObjekt.description;
+   imgGaleryItem.setAttribute('data-source',imgObjekt.original)
+   linkGaleryItem.append(imgGaleryItem);
+   divGaleryItem.append(linkGaleryItem);   
+   divGaleryArray.push(divGaleryItem); 
+   
+})
+
+const styleLiinks = document.createElement('link');
+styleLiinks.rel="stylesheet";
+styleLiinks.href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css";
+
+const scriptTag = document.createElement('script');
+scriptTag.src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"; 
 
 
- galleryBox.addEventListener('click', (etven) => {etven.preventDefault();
+galleryBox.append(...divGaleryArray);
 
-  const instance = basicLightbox.create(`<img src = ${etven.target.dataset.source} ">`)
-  instance.show();
+headStyleLiinks.append(styleLiinks);
 
- })
+bodyScriptref.append(scriptTag);
+
+
+  galleryBox.addEventListener('click', (etven) => {etven.preventDefault();
+
+   const instance = basicLightbox.create(`<img src = ${etven.target.dataset.source} ">`)
+   instance.show();
+
+  });
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,12 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const liGaleryArray = [];
 
- const galleryBox = document.querySelector(".gallery");
+galleryItems.map((imgObjekt) => {
+   const liGaleryItem = document.createElement('li');
+   const linkGaleryItem = document.createElement('a');
+   const imgGaleryItem = document.createElement('img');
+   linkGaleryItem.class="gallery__item";
+   linkGaleryItem.href=imgObjekt.original;
+   imgGaleryItem.src=imgObjekt.preview;
+   imgGaleryItem.alt=imgObjekt.description;
+   linkGaleryItem.append(imgGaleryItem);
+   liGaleryItem.append(linkGaleryItem);   
+   liGaleryArray.push(liGaleryItem); 
+})
 
 
- galleryBox.addEventListener('click', (etven) => {etven.preventDefault();
-     var lightbox = new SimpleLightbox('.gallery a ', { captionAttribute:'.img alt' });
+const galleryBox = document.querySelector(".gallery");
+
+galleryBox.append(...liGaleryArray);
+
+  galleryBox.addEventListener('click', (etven) => {etven.preventDefault();
+      var lightbox = new SimpleLightbox('.gallery a ', { captionAttribute:'.img alt' });
  
-     lightbox.show();
- })
+      lightbox.show();
+  })

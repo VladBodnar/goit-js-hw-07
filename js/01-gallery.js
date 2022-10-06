8c34edaa654a9b2493200a6898f56fc0664f2b4b
VladBodnar <104225683+VladBodnar@users.noreply.github.com>
2022-10-06 23:30:51 +0300

    final v3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
-const headStyleLiinks =document.querySelector('head');
-const bodyScriptref =document.querySelector('body');
+const headStyleLiinks = document.querySelector("head");
+const bodyScriptref = document.querySelector("body");
 const galleryBox = document.querySelector(".gallery");
-
 
 const divGaleryArray = [];
 
 galleryItems.map((imgObjekt) => {
-   const divGaleryItem = document.createElement('div');
-   const linkGaleryItem = document.createElement('a');
-   const imgGaleryItem = document.createElement('img');
-   divGaleryItem.class="gallery__item";
-   linkGaleryItem.class="gallery__link";
-   linkGaleryItem.href=imgObjekt.original;
-   imgGaleryItem.src=imgObjekt.preview;
-   imgGaleryItem.alt=imgObjekt.description;
-   imgGaleryItem.setAttribute('data-source',imgObjekt.original)
-   linkGaleryItem.append(imgGaleryItem);
-   divGaleryItem.append(linkGaleryItem);   
-   divGaleryArray.push(divGaleryItem); 
-   
-})
+  const divGaleryItem = document.createElement("div");
+  const linkGaleryItem = document.createElement("a");
+  const imgGaleryItem = document.createElement("img");
+  divGaleryItem.classList.add("gallery__item");
+  linkGaleryItem.classList.add("gallery__link");
+  imgGaleryItem.classList.add("gallery__image");
+  linkGaleryItem.href = imgObjekt.original;
+  imgGaleryItem.src = imgObjekt.preview;
+  imgGaleryItem.alt = imgObjekt.description;
+  imgGaleryItem.setAttribute("data-source", imgObjekt.original);
+  linkGaleryItem.append(imgGaleryItem);
+  divGaleryItem.append(linkGaleryItem);
+  divGaleryArray.push(divGaleryItem);
+});
 
-const styleLiinks = document.createElement('link');
-styleLiinks.rel="stylesheet";
-styleLiinks.href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css";
+const styleLiinks = document.createElement("link");
+styleLiinks.rel = "stylesheet";
+styleLiinks.href =
+  "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css";
 
-const scriptTag = document.createElement('script');
-scriptTag.src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"; 
-
+const scriptTag = document.createElement("script");
+scriptTag.src =
+  "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
 
 galleryBox.append(...divGaleryArray);
 
@@ -38,13 +38,15 @@ headStyleLiinks.append(styleLiinks);
 
 bodyScriptref.append(scriptTag);
 
+galleryBox.addEventListener("click", (etven) => {
+  etven.preventDefault();
+  console.log(etven.target.nodeName);
+  if (etven.target.nodeName !== "IMG") {
+    return;
+  }
 
-  galleryBox.addEventListener('click', (etven) => {etven.preventDefault();
-
-   const instance = basicLightbox.create(`<img src = ${etven.target.dataset.source} ">`)
-   instance.show();
-
-  });
-
-
-
+  const instance = basicLightbox.create(
+    `<img src = ${etven.target.dataset.source} ">`
+  );
+  instance.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const headStyleLiinks = document.querySelector("head");
+const bodyScriptref = document.querySelector("body");
+const galleryBox = document.querySelector(".gallery");
+
 const liGaleryArray = [];
 
 galleryItems.map((imgObjekt) => {
-   const liGaleryItem = document.createElement('li');
-   const linkGaleryItem = document.createElement('a');
-   const imgGaleryItem = document.createElement('img');
-   linkGaleryItem.class="gallery__item";
-   linkGaleryItem.href=imgObjekt.original;
-   imgGaleryItem.src=imgObjekt.preview;
-   imgGaleryItem.alt=imgObjekt.description;
-   linkGaleryItem.append(imgGaleryItem);
-   liGaleryItem.append(linkGaleryItem);   
-   liGaleryArray.push(liGaleryItem); 
-})
+  const liGaleryItem = document.createElement("li");
+  const linkGaleryItem = document.createElement("a");
+  const imgGaleryItem = document.createElement("img");
+  linkGaleryItem.classList.add("gallery__item");
+  linkGaleryItem.href = imgObjekt.original;
+  imgGaleryItem.src = imgObjekt.preview;
+  imgGaleryItem.alt = imgObjekt.description;
+  imgGaleryItem.classList.add("gallery__image");
+  linkGaleryItem.append(imgGaleryItem);
+  liGaleryItem.append(linkGaleryItem);
+  liGaleryArray.push(liGaleryItem);
+});
 
+const styleLiinks = document.createElement("link");
+styleLiinks.rel = "stylesheet";
+styleLiinks.href =
+  "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.10.3/simple-lightbox.css";
 
-const galleryBox = document.querySelector(".gallery");
+const scriptTag = document.createElement("script");
+scriptTag.src =
+  "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.10.3/simple-lightbox.min.js";
 
 galleryBox.append(...liGaleryArray);
+headStyleLiinks.append(styleLiinks);
+bodyScriptref.append(scriptTag);
 
-  galleryBox.addEventListener('click', (etven) => {etven.preventDefault();
-      var lightbox = new SimpleLightbox('.gallery a ', { captionAttribute:'.img alt' });
- 
-      lightbox.show();
-  })
+galleryBox.addEventListener("click", (etven) => {
+  etven.preventDefault();
+  var lightbox = new SimpleLightbox(".gallery a ", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+    captionSelector: "img",
+    captionType: "attr",
+    captionPosition: "bottom",
+    captionClass: "",
+  });
+
+  lightbox.show();
+});
